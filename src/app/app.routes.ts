e70d8ad799f323828d9b4978ed2ee7790f2869d8
Thenujana <chenarathenujana@gmail.com>
2025-04-29 14:02:40 +0530

    creation of dashboard,create employee components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { EmployeesComponent } from './pages/employees/employees.component';
 
 export const routes: Routes = [
     {
       path:"",
       component:DashboardComponent  
+    },
+    {
+        path:"employees",
+        component:EmployeesComponent
     }
 ];
---
+++ src/app/pages/add-employees/add-employees.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-employees',
+  imports: [],
+  templateUrl: './add-employees.component.html',
+  styleUrl: './add-employees.component.css'
+})
+export class AddEmployeesComponent {
+
+}
