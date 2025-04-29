0aa47fb756ec90caed13fa7a3df48d333f07b1e4
Thenujana <chenarathenujana@gmail.com>
2025-04-29 17:55:39 +0530

    app routes update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { EmployeesComponent } from './pages/employees/employees.component';
 import { AddEmployeesComponent } from './pages/add-employees/add-employees.component';
-import { UpdateEmployeesComponent } from './pages/update-employees/update-employees.component';
 
 export const routes: Routes = [
     {
